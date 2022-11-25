3fe97ee55ed214b3b7db7be42e59a914068f6ba4
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2022-11-25 17:26:57 -0300

    Ex de condição simples

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log('O console funcionou corretamente!')
+
+var vel = 60.5
+console.log(`A velocidade é de ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`Vc ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Use sempre o cinto de segurança`)
