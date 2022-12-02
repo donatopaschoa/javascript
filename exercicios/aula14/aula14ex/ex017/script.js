2b392b9195602c152e70c89252ee6e05a604ea9c
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2022-12-02 19:40:46 -0300

    JS: Exerc. while: tabuada, txtarea - JS

---
+++ exercicios/aula14/aula14ex/ex017/script.js
@@ -0,0 +1,26 @@
+function geratabuada() {
+    let tabuada = window.document.getElementById('txt_n')
+    let resultado = window.document.getElementById('txa_t')
+
+    //resultado.innerHTML = `Tabuada do ${t}` // teste
+
+    // Validação:
+    if (tabuada.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+
+        let t = Number(tabuada.value)
+        resultado.innerHTML = ""
+
+        for (let i = 1; i <= 10; i++) {
+            resultado.innerHTML += `${i} x ${t} = ${i * t} \n`
+        }
+    }
+}
+/*
+let tabuada = -8
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} x ${tabuada} = ${i * tabuada}`);
+}
+*/
