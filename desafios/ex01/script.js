65d96f64bb01c3e432c23657109e0a358ed8b93b
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2022-12-09 17:55:54 -0300

    JS: Exerc. desafio: mensagens

---
+++ desafios/ex01/script.js
@@ -0,0 +1,11 @@
+function mensagem1() {
+    window.alert('Você clicou no 1º botão!')
+}
+
+function mensagem2() {
+    window.alert('Você clicou no 2º botão!')
+}
+
+function mensagem3() {
+    window.alert('Você clicou no 3º botão!')
+}
