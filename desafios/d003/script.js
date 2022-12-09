3235f3c6c4d1f6b854023faf56c4488ca20bf4c0
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2022-12-09 19:18:54 -0300

    JS: Desafio: Antecessor e Sucessor

---
+++ desafios/d003/script.js
@@ -0,0 +1,7 @@
+function ant_suc() {
+    let num = window.prompt(`Digite um número inteiro qualquer`)
+
+    let n = Number(num)
+    window.alert(`Antes de ${n}, temos o número ${n-1}.\nDepois de ${n}, temos o número ${n + 1}.`)
+    
+}
