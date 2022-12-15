258f047c6e981b8912e2a97343a395102f95e19b
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2022-12-15 15:41:33 -0300

    JS: Modularização de JS: arq. .js externo

--- desafios/d014/script.js
+++ desafios/d014/script.js
@@ -1,10 +1,10 @@
-import * as funcoesGerais from '.funcoesGerais.js';
+import * as funcoes from '.funcoesGerais.js';
 function formatar() {
     let n = Number(window.prompt('Digite um número inteiro'))
     let res = window.document.querySelector('div#res')
 
     //res.innerHTML = `O número ${n} formatado em real = ${formatacaoBR(n)}`
-    res.innerHTML = `O número ${n} formatado em real = ${funcoesGerais.formatacaoBR(n)}`
+    res.innerHTML = `O número ${n} formatado em real = ${funcoes.formatacaoBR(n)}`
 }
 /*
 function formatacaoBR(n) {
